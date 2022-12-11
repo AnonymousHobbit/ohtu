2bf93d7375342bba00b4a68e8c1bf992c2db7189
AnonymousHobbit <30089493+AnonymousHobbit@users.noreply.github.com>
2022-12-11 20:01:06 +0200

    Finish exercises 1-5 week 6

---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,22 @@
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
+
+class QueryBuilder:
+    def __init__(self, query=All()):
+        self.query = query
+
+    def build(self):
+        return self.query
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self.query, PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self.query, HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self.query, HasFewerThan(value, attr)))
+
+    def oneOf(self, *matchers):
+        return QueryBuilder(Or(*matchers))
+
+    
